0a58f7a24bd112e617dee1b1f0d89b128def59fa
richard <richardribucanp@gmail.com>
2018-03-07 13:49:37 -1000

    DONE

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var array = ["stuff", "other things", "cars", "memes"];
+var array = ["stuff", 2, "cars", "memes"];
 
-var rep = "really";
+var rep = "i replaced the rear one with this one, check out the code lmao";
 
 array[3] = rep;
 
@@ -8,6 +8,21 @@ var i;
 for(i=0; i<=3; i++){
   var a = document.getElementById("fourth")
   var b = document.createElement("div")
-  b.innerHTML = array
+  b.innerHTML = array[i]
   a.appendChild(b)
 }
+
+var prop = document.getElementById("fourth")
+prop.style.fontFamily = "arial";
+prop.style.color = "blue";
+prop.style.background = "red";
+
+
+var c = Math.floor(Math.random()*3)
+
+var d = document.getElementById("paragraph3")
+var e = document.createElement("div")
+
+e.innerHTML = array[c]
+
+d.appendChild(e)
